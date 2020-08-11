6551eeb72807132ea24c1b4a3176e591905a4ce1
Simas <simas@simas-dei.com>
2020-08-11 13:29:16 +0300

    add moderation ms handling

--- index.js
+++ index.js
@@ -39,8 +39,27 @@ app.post('/posts/:id/comments', async (req, res) => {
 	res.status(201).send(comments);
 });
 
-app.post('/events', (req, res) => {
-	console.log(`Received Event: ${req.body.type}`);
+app.post('/events', async(req, res) => {
+	const { type, data} = req.body;
+	console.log(`Received Event: ${type}`);
+
+	if ( type === 'CommentModerated') {
+		const { postId, id, status, content } = data;
+
+		const comments = commentsByPostId[postId];
+		const comment = comments.find(item => item.id === id);
+		comment.status = status;
+
+		await axios.post('http://localhost:4005/events', {
+			type: 'CommentUpdated',
+			data: {
+				id,
+				postId,
+				status,
+				content
+			}
+		})
+	}
 
 	res.send({});
 });
